def5d40c2ea53d598163767a2e82765733c61ac6
amicolon <amadeusz.rogowski@gmail.com>
2018-12-27 23:14:42 +0100

    [tests] loading test data to database

--- src/main/java/com/amicolon/bootstrap/InitialDataRunner.java
+++ src/main/java/com/amicolon/bootstrap/InitialDataRunner.java
@@ -1,16 +1,15 @@
 package com.amicolon.bootstrap;
 
-import com.amicolon.domain.Priority;
-import com.amicolon.domain.State;
+import com.amicolon.domain.*;
 import com.amicolon.domain.enumerated.PriorityName;
 import com.amicolon.domain.enumerated.StateName;
-import com.amicolon.repositories.PriorityRepository;
-import com.amicolon.repositories.StateRepository;
+import com.amicolon.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -21,15 +20,60 @@ public class InitialDataRunner implements ApplicationRunner
 	private final StateRepository stateRepository;
 	private final PriorityRepository priorityRepository;
 
+	private final LabelRepository labelRepository;
+	private final TaskRepository taskRepository;
+
+	private final CategoryRepository categoryRepository;
+
 	@Autowired
-	public InitialDataRunner(StateRepository stateRepository, PriorityRepository priorityRepository)
+	public InitialDataRunner(StateRepository stateRepository, PriorityRepository priorityRepository, LabelRepository labelRepository, TaskRepository taskRepository, CategoryRepository categoryRepository)
 	{
 		this.stateRepository = stateRepository;
 		this.priorityRepository = priorityRepository;
+		this.labelRepository = labelRepository;
+		this.taskRepository = taskRepository;
+		this.categoryRepository = categoryRepository;
 	}
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception
+	{
+		prepareEnums();
+
+		List<Category> categoryList = asList(prepareCategory("Main category 1"),
+				prepareCategory("Main category 2"),
+				prepareCategory("Main category 3"));
+
+		categoryRepository.saveAll(categoryList);
+	}
+
+	private Category prepareCategory(String catName)
+	{
+		Task task = new Task();
+		task.setTitle("test-task");
+		task.setDescription("making-test");
+
+		Label school = new Label();
+		school.setLabelName("school");
+			labelRepository.save(school);
+//		task.getLabels().add(school);
+
+		task.setPriority(priorityRepository.findById(3L).get());
+
+		task.setState(stateRepository.findById(2L).get());
+
+		task.setStartDate(LocalDate.now());
+
+		taskRepository.save(task);
+
+		Category category = new Category();
+		category.setCategoryName(catName);
+//		category.addTask(task);
+
+		return category;
+	}
+
+	private void prepareEnums()
 	{
 		List<StateName> states = asList(StateName.values());
 
