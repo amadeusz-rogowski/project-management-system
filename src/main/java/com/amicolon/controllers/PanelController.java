68fa2310d772d54b3ff0aa9c37c6df6e339bee80
amicolon <amadeusz.rogowski@gmail.com>
2018-12-27 23:14:06 +0100

    Establishing panel's view

--- src/main/java/com/amicolon/controllers/PanelController.java
+++ src/main/java/com/amicolon/controllers/PanelController.java
@@ -1,15 +1,29 @@
 package com.amicolon.controllers;
 
+import com.amicolon.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PanelController
 {
 
-    @RequestMapping({"", "/", "/panel"})
-    public String getPanelPage()
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public PanelController(CategoryRepository categoryRepository)
     {
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    @RequestMapping({"", "/", "/panel"})
+    public String getPanelPage(Model model)
+    {
+        model.addAttribute("categories", categoryRepository.findAll());
+
         return "panel";
     }
 
