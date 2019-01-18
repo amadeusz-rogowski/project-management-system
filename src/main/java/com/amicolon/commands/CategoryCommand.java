e1d65b636b0c07409cc37738062ef5e8c0442325
amicolon <amadeusz.rogowski@gmail.com>
2019-01-18 23:47:14 +0100

    [command] adding builders to command classes

--- src/main/java/com/amicolon/commands/CategoryCommand.java
+++ src/main/java/com/amicolon/commands/CategoryCommand.java
@@ -1,16 +1,16 @@
 package com.amicolon.commands;
 
 import com.amicolon.domain.Task;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CategoryCommand
 {
 	private Long id;
--- src/main/java/com/amicolon/commands/TaskCommand.java
+++ src/main/java/com/amicolon/commands/TaskCommand.java
@@ -1,20 +1,19 @@
 package com.amicolon.commands;
 
-import com.amicolon.domain.Category;
 import com.amicolon.domain.Label;
 import com.amicolon.domain.Priority;
 import com.amicolon.domain.State;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class TaskCommand
 {
 	private Long id;
@@ -25,5 +24,5 @@ public class TaskCommand
 	private State state;
 	private Priority priority;
 	private Set<Label> labels = new HashSet<>();
-	private Category category;
+	private Long categoryId;
 }
